de6020c385c9f03d42d8b1e7d449e9a47805a7f6
shubhanshjaiswal <codingbrains36@gmail.com>
2024-05-09 20:52:21 +0530

    chanages for the blogs and y Choose us?

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,14 @@
-$(function () {
-    $("#nav-placeholder").load("navbar.html");
+window.addEventListener('DOMContentLoaded', function () {
+    var sliderContainer = document.getElementById('sliderContainer');
+    var swipercontainer = document.getElementById('swiper-container');
+    if (sliderContainer) {
+
+        sliderContainer.addEventListener('mouseenter', function () {
+            scrollToSection();
+        });
+    }
+    function scrollToSection() {
+        var yOffset = sliderContainer.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({ top: yOffset, behavior: 'smooth' });
+    }
 });
